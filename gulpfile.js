873580edcf12c2f406614bdc07493385d93e20eb
Anya Vigolajnen <avi26.job@yandex.ru>
2020-04-22 13:54:55 +0300

    правки - форма, подключение скриптов

--- gulpfile.js
+++ gulpfile.js
@@ -1,121 +1,127 @@
-"use strict";
+'use strict';
 
-var gulp = require("gulp");
-var plumber = require("gulp-plumber");
-var sourcemap = require("gulp-sourcemaps");
-var sass = require("gulp-sass");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
-var server = require("browser-sync").create();
-var csso = require("gulp-csso");
-var rename = require("gulp-rename");
-var imagemin = require("gulp-imagemin");
-var webp = require("gulp-webp");
-var svgstore = require("gulp-svgstore")
-var posthtml = require("gulp-posthtml");
-var include = require("posthtml-include");
-var del = require("del");
-var uglify = require("gulp-uglify");
-var rigger = require("gulp-rigger");
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var sourcemap = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var server = require('browser-sync').create();
+var csso = require('gulp-csso');
+var rename = require('gulp-rename');
+var imagemin = require('gulp-imagemin');
+var webp = require('gulp-webp');
+var svgstore = require('gulp-svgstore')
+var posthtml = require('gulp-posthtml');
+var include = require('posthtml-include');
+var del = require('del');
+var uglify = require('gulp-uglify');
+var rigger = require('gulp-rigger');
+var concat = require('gulp-concat');
 
-gulp.task("css", function () {
-  return gulp.src("source/sass/style.scss")
+gulp.task('css', function () {
+  return gulp.src('source/sass/style.scss')
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(sass())
     .pipe(postcss([ autoprefixer() ]))
-    .pipe(gulp.dest("build/css"))
+    .pipe(gulp.dest('build/css'))
     .pipe(csso())
-    .pipe(rename("style.min.css"))
-    .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("build/css"))
+    .pipe(rename('style.min.css'))
+    .pipe(sourcemap.write('.'))
+    .pipe(gulp.dest('build/css'))
     .pipe(server.stream());
 });
 
-gulp.task("server", function () {
+gulp.task('server', function () {
   server.init({
-    server: "build/",
+    server: 'build/',
     notify: false,
     open: true,
     cors: true,
     ui: false
   });
 
-  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
-  gulp.watch("source/*.html", gulp.series("html", "refresh"));
-  gulp.watch("source/js/**/*.js", gulp.series("js", "refresh"));
+  gulp.watch('source/sass/**/*.{scss,sass}', gulp.series('css'));
+  gulp.watch('source/img/icon-*.svg', gulp.series('sprite', 'html', 'refresh'));
+  gulp.watch('source/*.html', gulp.series('html', 'refresh'));
+  gulp.watch('source/js/**/*.js', gulp.series('js', 'refresh'));
 });
 
-gulp.task("refresh", function (done) {
+gulp.task('refresh', function (done) {
   server.reload();
   done();
 });
 
-gulp.task("images", function() {
-  return gulp.src("source/img/**/*.{png,jpg,svg}")
+gulp.task('images', function() {
+  return gulp.src('source/img/**/*.{png,jpg,svg}')
     .pipe(imagemin([
       imagemin.optipng({optimizationLevel: 3}),
       imagemin.jpegtran({progressive: true}),
       imagemin.svgo()
     ]))
 
-    .pipe(gulp.dest("source/img"));
+    .pipe(gulp.dest('source/img'));
 
 });
 
-gulp.task("webp", function () {
-  return gulp.src("source/img/**/*.{png,jpg}")
+gulp.task('webp', function () {
+  return gulp.src('source/img/**/*.{png,jpg}')
     .pipe(webp({quality: 90}))
-    .pipe(gulp.dest("source/img"));
+    .pipe(gulp.dest('source/img'));
 });
 
-gulp.task("sprite", function () {
-  return gulp.src("source/img/icons/{icon-*,htmlacademy*}.svg")
+gulp.task('sprite', function () {
+  return gulp.src('source/img/icons/{icon-*,htmlacademy*}.svg')
     .pipe(svgstore({inlineSvg: true}))
-    .pipe(rename("sprite_auto.svg"))
-    .pipe(gulp.dest("build/img"));
+    .pipe(rename('sprite_auto.svg'))
+    .pipe(gulp.dest('build/img'));
 });
 
-gulp.task("html", function () {
-  return gulp.src("source/*.html")
+gulp.task('html', function () {
+  return gulp.src('source/*.html')
     .pipe(posthtml([
       include()
     ]))
-    .pipe(gulp.dest("build"));
+    .pipe(gulp.dest('build'));
 });
 
-gulp.task("copy", function () {
+gulp.task('copy', function () {
   return gulp.src([
-    "source/fonts/**/*.{woff,woff2}",
-    "source/img/**",
-    "source//*.ico"
+    'source/fonts/**/*.{woff,woff2}',
+    'source/img/**',
+    'source//*.ico'
     ], {
-      base: "source"
+      base: 'source'
     })
-  .pipe(gulp.dest("build"));
+  .pipe(gulp.dest('build'));
 });
 
-gulp.task("js", function () {
-  return gulp.src("source/js/script.js")
+gulp.task('js', function () {
+  return gulp.src('source/js/script.js')
     .pipe(plumber())
-    .pipe(rigger())
-  .pipe(gulp.dest("build/js"))
+  .pipe(gulp.dest('build/js'))
 });
 
-gulp.task("libs", function () {
-  return gulp.src("source/js/vendor.js")
+gulp.task('libs', function () {
+  return gulp.src('source/js/vendor.js')
     .pipe(plumber())
     .pipe(rigger())
-    .pipe(gulp.dest("build/js"))
-    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest('build/js'))
+    .pipe(rename({ suffix: '.min' }))
     .pipe(uglify())
-    .pipe(gulp.dest("build/js"));
+    .pipe(gulp.dest('build/js'));
 });
 
-gulp.task("clean", function () {
-  return del("build");
+gulp.task('scripts', function() {
+  return gulp.src(['./node_modules/svg4everybody/dist/svg4everybody.min.js', 'source/js/vendor.js'])
+    .pipe(concat('vendor.min.js'))
+    .pipe(gulp.dest('build/js'));
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html", "js", "libs"));
-gulp.task("start", gulp.series("build", "server"));
+gulp.task('clean', function () {
+  return del('build');
+});
+
+gulp.task('build', gulp.series('clean', 'copy', 'css', 'scripts', 'sprite', 'html', 'js'));
+gulp.task('start', gulp.series('build', 'server'));
--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,4 +1,4 @@
 'use strict';
 // = ../../node_modules/pixel-glass/script.js
-//= ../../node_modules/svg4everybody/dist/svg4everybody.js
+
 svg4everybody();
